858f685ec580e121af0d091673dd8b9b849fe452
Sujan Madhur <madhursujan@gmail.com>
2023-01-10 13:16:45 +0530

    script modified

--- challenge_3/script.sh
+++ challenge_3/script.sh
@@ -1,11 +1,12 @@
 #!bin/bash
 
 key=$1 #give input json filename ex: input.json
+filename=$2
 
 fetchValue () {
-   query=`cat $1 | sed -e 's/\//./g' -e 's/^/./'`
+   query=`echo $1 | sed -e 's/\//./g' -e 's/^/./'`
    echo $query
-   jq '$query' $1
+   jq '$query' $2
 }
 
-fetchValue $key
+fetchValue $key $filename
